fed89e1101944a36783c0f710f7a16f2cd0bda7b
GeekEast <xiang.tan.p2109@gmail.com>
2020-02-20 23:02:45 +1030

    update

--- index.ts
+++ index.ts
@@ -2,32 +2,34 @@ import _ from "lodash";
 import now from 'performance-now';
 import { update } from 'immutable';
 import produce from 'immer';
-import { set } from 'lodash/fp';
+import { update as lpdate } from 'lodash/fp';
 import Immutable from 'seamless-immutable';
+import { generateDate, getPerformance } from "./utils";
 
-const generateDate = (count) => {
-  const arr = _.map(_.range(1, count), item => {
-    return { key: item, title: `task ${item}`, complete: false }
-  });
-  return _.keyBy(arr, 'key');
-}
-
-const getPerformance = (callback, ...params) => {
-  const start = now();
-  callback(...params)
-  const end = now();
-  return (end - start).toFixed(3)
-}
-
-const es5Reducer = (todos, id) => { return { ...todos, [id]: { ...todos[id], complete: !todos[id].complete } } }
+// 不可变库updater
+const es6Reducer = (todos, id) => { return { ...todos, [id]: { ...todos[id], complete: !todos[id].complete } } }
 const immutableJsReducer = (todos, id) => update(todos, id, (val: any) => update(val, 'complete', v => !v))
 const immerReducer = (todos, id) => produce(todos, draft => { draft[id].complete = true })
-const lodashReducer = (todos, id) => set([id, 'complete'], true, todos)
-const seamlessImmutableReducer = (todos, id) => Immutable(todos).setIn([id, 'complete'], true);
+const lodashReducer = (todos, id) => lpdate([id, 'complete'], x => !x, todos)
+const seamlessImmutableReducer = (todos, id) => Immutable(todos).updateIn([id, 'complete'], x => !x);
 
-
+// 初始化数据
 const data = generateDate(100000);
 
+// immutable是否满足检测:
+// const es6G = es6Reducer(data, '5000');
+// const immutableG = immutableJsReducer(data, '5000');
+// const immerG = immerReducer(data, '5000') as Object;
+// const lodashG = lodashReducer(data, '5000');
+// const seamlessG = seamlessImmutableReducer(data, '5000')
+// console.log(es6G === data);
+// console.log(immutableG === data);
+// console.log(immerG === data);
+// console.log(lodashG === data);
+// console.log(seamlessG === data);
+
+
+// 性能测试:
 const getAverage = (count, reducer, data, id) => {
   const res: any = [];
   for (let i = 0; i < count; i++) {
@@ -37,7 +39,7 @@ const getAverage = (count, reducer, data, id) => {
   console.log(reducer.name, ': ', _.mean(res));
 }
 
-getAverage(10, es5Reducer, data, '5000')
+getAverage(10, es6Reducer, data, '5000')
 getAverage(10, lodashReducer, data, '5000')
 getAverage(10, immutableJsReducer, data, '5000')
 getAverage(10, immerReducer, data, '5000')
---
+++ utils.ts
@@ -0,0 +1,16 @@
+import now from 'performance-now';
+import _ from 'lodash';
+
+export const generateDate = (count) => {
+  const arr = _.map(_.range(1, count), item => {
+    return { key: item, title: `task ${item}`, complete: false }
+  });
+  return _.keyBy(arr, 'key');
+}
+
+export const getPerformance = (callback, ...params) => {
+  const start = now();
+  callback(...params)
+  const end = now();
+  return (end - start).toFixed(3)
+}
