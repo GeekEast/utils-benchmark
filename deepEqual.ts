7d9ab55f874e85f77fb05ef5849c074eda6d31a4
GeekEast <xiang.tan.p2109@gmail.com>
2020-02-23 14:23:53 +1030

    udpate

--- deepEqual.ts
+++ deepEqual.ts
@@ -9,3 +9,10 @@ const data2 = generateDate(1000000);
 console.log('lodash/isEqual ', getPerformance(_.isEqual, data1, data2))
 console.log('fast-deep-equal: ', getPerformance(equal, data1, data2))
 console.log('react-fast-compare:  ', getPerformance(isEqual, data1, data2))
+
+// 对Array同样有效
+console.log(isEqual([[[]]],[[[1]]]));
+// 对function无效，保证function immutable
+console.log(isEqual(()=>{}, ()=>{}))
+
+
